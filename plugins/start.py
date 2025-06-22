edd8f830fd7ae62941aafb1fa4aa8210bb367ff6
MOHIT5145 <uvrohit004@gmail.com>
2025-06-22 15:17:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -163,7 +163,7 @@ async def start_command(client: Client, message: Message):
                 btn = [
                     [InlineKeyboardButton("Verify here", url=link)],
                    [InlineKeyboardButton('How to use the bot', url=TUT_VID)],
-                    [InlineKeyboardButton('JOIN nOW', url=@MOTUMOVIES)]
+                    [InlineKeyboardButton('JOIN NOW', url=https://t.me/motulinks)]
                     
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
